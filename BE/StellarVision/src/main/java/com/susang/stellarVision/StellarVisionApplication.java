b0af8aed46a041e03fb4189b3c4be6e11370619a
wnsduq4258 <wnsduq4258@naver.com>
2025-07-24 17:48:04 +0900

    init project

---
+++ BE/StellarVision/src/main/java/com/susang/stellarVision/StellarVisionApplication.java
@@ -0,0 +1,13 @@
+package com.susang.stellarVision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellarVisionApplication.class, args);
+	}
+
+}
---
+++ BE/StellarVision/src/test/java/com/susang/stellarVision/StellarVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.susang.stellarVision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
