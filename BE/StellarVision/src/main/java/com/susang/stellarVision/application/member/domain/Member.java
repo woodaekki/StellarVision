bf675e448d9cd23b51077ff5dda2a71c250cc56f
wnsduq4258 <wnsduq4258@naver.com>
2025-07-28 12:06:07 +0900

    chore: 프로젝트 초기 ENTITY 추가

---
+++ BE/StellarVision/src/main/java/com/susang/stellarVision/application/member/domain/Member.java
@@ -0,0 +1,73 @@
+package com.susang.stellarVision.application.member.domain;
+
+import com.susang.stellarVision.application.profile.domain.Profile;
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false, unique = true, length = 20)
+    private String email;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 10)
+    private String nickname;
+
+    @Column(nullable = false)
+    private LocalDate birth;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Setter
+    @Column(name = "latest_login")
+    private LocalDateTime latestLogin;
+
+    @Column(name = "follower_count", nullable = false)
+    private Long followerCount;
+
+    @Column(name = "following_count", nullable = false)
+    private Long followingCount;
+
+    @Setter
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_id")
+    private Profile profile;
+
+    @Builder
+    public Member(String email, String password, String nickname, LocalDate birth,
+            LocalDateTime createdAt, LocalDateTime latestLogin, Long followerCount,
+            Long followingCount, Boolean isDeleted, Profile profile) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.birth = birth;
+        this.createdAt = createdAt;
+        this.latestLogin = latestLogin;
+        this.followerCount = followerCount;
+        this.followingCount = followingCount;
+        this.isDeleted = isDeleted;
+        this.profile = profile;
+    }
+}
---
+++ BE/StellarVision/src/main/java/com/susang/stellarVision/application/profile/domain/Profile.java
@@ -0,0 +1,46 @@
+package com.susang.stellarVision.application.profile.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "profile_s3_key", nullable = false, unique = true, length = 20)
+    private String profileS3Key;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(name = "is_gallery_public", nullable = false)
+    private Boolean isGalleryPublic;
+
+    @Column(name = "is_video_public", nullable = false)
+    private Boolean isVideoPublic;
+
+    @Column(name = "is_collection_public", nullable = false)
+    private Boolean isCollectionPublic;
+
+    @Builder
+    public Profile(String profileS3Key, String description,
+            Boolean isGalleryPublic, Boolean isVideoPublic, Boolean isCollectionPublic) {
+        this.profileS3Key = profileS3Key;
+        this.description = description;
+        this.isGalleryPublic = isGalleryPublic;
+        this.isVideoPublic = isVideoPublic;
+        this.isCollectionPublic = isCollectionPublic;
+    }
+}
