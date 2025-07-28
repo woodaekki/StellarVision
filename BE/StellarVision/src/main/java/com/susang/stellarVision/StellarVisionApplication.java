623d5c1796c2238c4a7e87a94d0c9eee57388495
wnsduq4258 <wnsduq4258@naver.com>
2025-07-28 14:30:20 +0900

    feature: BaseEntity 생성

--- BE/StellarVision/src/main/java/com/susang/stellarVision/StellarVisionApplication.java
+++ BE/StellarVision/src/main/java/com/susang/stellarVision/StellarVisionApplication.java
@@ -2,8 +2,10 @@ package com.susang.stellarVision;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StellarVisionApplication {
 
 	public static void main(String[] args) {
