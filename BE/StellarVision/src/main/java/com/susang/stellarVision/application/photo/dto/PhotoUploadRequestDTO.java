6c93df6a8720762f192a2e7ebc3dd5b16659be60
구인완 <dlsdhks78@naver.com>
2025-07-30 10:40:37 +0900

    [Feature] #82 사진 업로드, 조회 API 구현

--- BE/StellarVision/src/main/java/com/susang/stellarVision/application/photo/dto/PhotoUploadRequestDTO.java
+++ BE/StellarVision/src/main/java/com/susang/stellarVision/application/photo/dto/PhotoUploadRequestDTO.java
@@ -3,7 +3,7 @@ package com.susang.stellarVision.application.photo.dto;
 import lombok.Getter;
 
 @Getter
-public class PhotoUploadRequest {
+public class PhotoUploadRequestDTO {
     private Long memberId;
     private String originalFilename;
 }
