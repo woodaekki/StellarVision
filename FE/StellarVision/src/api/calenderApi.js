78afa32f863c4f3ca7dc627da5c3ef13913cd31b
kimseonwoo <dora01622@naver.com>
2025-08-01 13:06:33 +0900

    feature:갤러리 업로드 및 레이아웃 기능 도중

--- FE/StellarVision/src/api/calenderApi.js
+++ FE/StellarVision/src/api/calenderApi.js
@@ -1,5 +1,11 @@
-// .env 파일을 안 만들어서 데이터 정보 띄우는 것만 보고, 키는 삭제할 예정
-export const SERVICE_KEY = '';
+// // .env 파일을 안 만들어서 데이터 정보 띄우는 것만 보고, 키는 삭제할 예정
+// export const SERVICE_KEY = '';
+
+// export function getAstroEventInfoUrl(solYear, solMonth) {
+//   return `http://apis.data.go.kr/B090041/openapi/service/AstroEventInfoService/getAstroEventInfo?solYear=${solYear}&solMonth=${solMonth}&ServiceKey=${SERVICE_KEY}`;
+// }
+
+const SERVICE_KEY = import.meta.env.VITE_SERVICE_KEY;
 
 export function getAstroEventInfoUrl(solYear, solMonth) {
   return `http://apis.data.go.kr/B090041/openapi/service/AstroEventInfoService/getAstroEventInfo?solYear=${solYear}&solMonth=${solMonth}&ServiceKey=${SERVICE_KEY}`;
