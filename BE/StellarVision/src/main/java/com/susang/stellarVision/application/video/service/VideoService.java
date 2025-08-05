0698deb38080579c4e32994ad2f5bf3983758b14
구인완 <dlsdhks78@naver.com>
2025-08-05 12:01:26 +0900

    [Feature]  #116,117 다시보기 검색, 다시보기 전체목록에서 조회 구현

--- BE/StellarVision/src/main/java/com/susang/stellarVision/application/video/service/VideoService.java
+++ BE/StellarVision/src/main/java/com/susang/stellarVision/application/video/service/VideoService.java
@@ -3,6 +3,7 @@ package com.susang.stellarVision.application.video.service;
 
 
 import com.susang.stellarVision.application.video.dto.VideoResponse;
+import com.susang.stellarVision.application.video.dto.VideoSearchRequest;
 import com.susang.stellarVision.application.video.dto.VideoTagListResponse;
 import com.susang.stellarVision.application.video.dto.VideoTagRequest;
 import com.susang.stellarVision.application.video.dto.VideoUpdateRequest;
@@ -17,8 +18,11 @@ public interface VideoService {
     String getVideoPresignedUrl(Long videoId) throws VideoNotFoundException;
     void deleteVideo(Long videoId) throws VideoNotFoundException;
     Page<VideoResponse> getVideosByMemberId(Long memberId, Pageable pageable);
+    Page<VideoResponse> getVideos(Pageable pageable);
+    Page<VideoResponse> searchVideos(VideoSearchRequest condition, Pageable pageable);
     void addVideoTag(Long videoId,VideoTagRequest videoTagRequest) throws VideoNotFoundException;
     VideoTagListResponse getTagsByVideoId(Long videoId) throws VideoNotFoundException;
     void deleteVideoTag(Long videoId, Long tagId) throws VideoNotFoundException , VideoTagNotFoundException;
     void updateVideoContent(Long videoId,VideoUpdateRequest videoUpdateRequest) throws VideoNotFoundException;
+
 }
