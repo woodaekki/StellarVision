158773e4da2ed6ae7379312a96798f0aa49da7d3
구인완 <dlsdhks78@naver.com>
2025-08-06 12:12:24 +0900

    [Feature] #139 다시보기 업로드 메서드 구현

---
+++ BE/StellarVision/src/main/java/com/susang/stellarVision/application/video/repository/ThumbnailRepository.java
@@ -0,0 +1,8 @@
+package com.susang.stellarVision.application.video.repository;
+
+import com.susang.stellarVision.entity.Thumbnail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ThumbnailRepository extends JpaRepository<Thumbnail, Long> {
+
+}
