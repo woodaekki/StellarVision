0bbedb394f9f8358e5c55123548ef6288314ca28
양정현 <didrn43@naver.com>
2025-08-17 16:58:36 +0900

    Feature/ai/24 update detection model

--- FE/StellarVision/src/api/calenderApi.js
+++ FE/StellarVision/src/api/calenderApi.js
@@ -8,5 +8,5 @@
 const SERVICE_KEY = import.meta.env.VITE_SERVICE_KEY;
 
 export function getAstroEventInfoUrl(solYear, solMonth) {
-  return `http://apis.data.go.kr/B090041/openapi/service/AstroEventInfoService/getAstroEventInfo?solYear=${solYear}&solMonth=${solMonth}&ServiceKey=${SERVICE_KEY}`;
+  return `https://apis.data.go.kr/B090041/openapi/service/AstroEventInfoService/getAstroEventInfo?solYear=${solYear}&solMonth=${solMonth}&ServiceKey=${SERVICE_KEY}`;
 }
